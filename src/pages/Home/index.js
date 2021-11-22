a2559b131a9af92f17316190ffdfd551a5c3620a
Ivan Matt <ivanmatt1995@gmail.com>
2021-11-22 08:27:06 -0400

    :sparkles: Import dos components e images

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,6 +3,14 @@ import "./style.css";
 import Logo from "../../assets/Logo.png";
 import Barber from "../../assets/barber.png";
 import Sair from "../../assets/Sair.png";
+import Card from "../../components/Card";
+import Leonardo from "../../assets/imageLeonardo.png";
+import Cleiton from "../../assets/imageCleiton.png";
+import Rolangela from "../../assets/imageRolangela.png";
+import Robson from "../../assets/imageRobson.png";
+import Borges from "../../assets/imageBorges.png";
+import Diego from "../../assets/imageDiego.png";
+import { CgStopwatch } from "react-icons/cg";
 
 function Home() {
   return (
@@ -30,8 +38,52 @@ function Home() {
           <h1>Horarios agendados </h1>
           <p>Hoje | Dia 06 | Segunda-Feira</p>
         </div>
+        <div className="contentAtendimento">
+          <h4>Atendimento a Seguir</h4>
+          <div className="cardAtendimento">
+            <span>
+              <img src={Leonardo} alt="imagem de cliente" />
+              <p>Leonardo Minatti</p>
+            </span>
+            <p>
+              <CgStopwatch className="iconRelogio" /> 08:00
+            </p>
+          </div>
+        </div>
         <div className="wrapperCalendario">
-          <h4>React Suite Calendar Component</h4>
+          <div className="horarioManha">
+            <p>Manha</p>
+            <span></span>
+            <div>
+              <CgStopwatch className="iconRelogio" />
+              <p>08:30</p> <Card img={Cleiton} name="Cleiton Souza" />
+            </div>
+            <div>
+              <CgStopwatch className="iconRelogio" />
+              <p>11:00</p>
+              <Card img={Rolangela} name="Rolangela B. Rusena" />
+            </div>
+          </div>
+          <div className="horarioTarde">
+            <p>Tarde</p>
+            <span></span>
+            <div>
+              <CgStopwatch className="iconRelogio" />
+              <p>13:00</p>
+              <Card img={Robson} name="Robson Marquezine" />
+            </div>
+            <div>
+              <CgStopwatch className="iconRelogio" />
+
+              <p>15:00</p>
+              <Card img={Borges} name="Borges Alfredo" />
+            </div>
+            <div>
+              <CgStopwatch className="iconRelogio" />
+              <p>17:30</p>
+              <Card img={Diego} name="Diego Fernandes" />
+            </div>
+          </div>
         </div>
       </div>
     </div>
